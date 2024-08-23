2c0566262647d07774e18f15cc47dd1d5697c404
chetana.lotliker <chetanalotliker@gmail.com>
2024-08-23 16:39:21 +0530

    Update the latest changes as per the package

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const greet = require("greet-creativec");
+const express = require("express");
 
-greet("John Snow"); 
+const app = express();
+const PORT = 8085;
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}/`)
+)
+
+app.get('/', (req, res) => {
+    const name = req.query.name ? req.query.name : 'John smith';
+    const message = greet(name)
+    return res.status(200).send(message)
+});
